5c9886c9bf64072cde450144e1b82c99a3c96d8e
Elie Hymowitz <ehymowitz@gmail.com>
2024-12-11 18:52:36 -0500

    feat: fireship tutorial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,77 @@ import ThreeScene from "@/components/threeScene";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen font-white bg-transparent w-5 ">
       <main>
+        <div className="absolute font-white bg-transparent">
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+          <p>TEST COPY TEXT</p>
+        </div>
         <ThreeScene />
       </main>
     </div>
--- src/components/threeScene.tsx
+++ src/components/threeScene.tsx
@@ -3,57 +3,108 @@
 import React, { useRef, useEffect } from "react";
 import * as THREE from "three";
 
+import { OrbitControls } from "three/examples/jsm/Addons.js";
+
 const ThreeScene: React.FC = () => {
   const containerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
-      // Initialize Three.js scene here
+      // init scene, camera, renderer
       const scene = new THREE.Scene();
+
       const camera = new THREE.PerspectiveCamera(
         75,
         window.innerWidth / window.innerHeight,
         0.1,
         1000
       );
+      camera.position.setZ(30);
+
       const renderer = new THREE.WebGLRenderer();
-      // Render the scene and camera
-      containerRef.current?.appendChild(renderer.domElement);
-      camera.position.z = 5;
-
-      const geometry = new THREE.BoxGeometry();
-      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-      const cube = new THREE.Mesh(geometry, material);
-      scene.add(cube);
-
-      renderer.render(scene, camera);
       renderer.setSize(window.innerWidth, window.innerHeight);
+      renderer.setPixelRatio(window.devicePixelRatio);
+      containerRef.current?.appendChild(renderer.domElement);
 
-      const renderScene = () => {
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+      // lighting
+      const pointlight = new THREE.PointLight("white");
+      pointlight.position.set(0, 0, 0);
+      scene.add(pointlight);
+
+      const ambientLight = new THREE.AmbientLight("white");
+      scene.add(ambientLight);
+
+      // helper
+      const lightHelper = new THREE.PointLightHelper(pointlight);
+      scene.add(lightHelper);
+
+      const gridHelper = new THREE.GridHelper(200, 50);
+      scene.add(gridHelper);
+
+      // orbit controls
+      const orbitControl = new OrbitControls(camera, renderer.domElement);
+
+      // geometry
+      const torusGeo = new THREE.TorusGeometry(10, 3, 16, 100);
+      const torusMat = new THREE.MeshStandardMaterial({
+        color: "tomato",
+      });
+      const torusMesh = new THREE.Mesh(torusGeo, torusMat);
+      scene.add(torusMesh);
+
+      // add stars
+      const addStar = () => {
+        const starGeo = new THREE.SphereGeometry(0.25, 24, 24);
+        const starMat = new THREE.MeshStandardMaterial({ color: "white" });
+        const star = new THREE.Mesh(starGeo, starMat);
+
+        const [x, y, z] = Array(3)
+          .fill(0)
+          .map(() => THREE.MathUtils.randFloatSpread(100));
+        star.position.set(x, y, z);
+        scene.add(star);
+      };
+      Array(200).fill(0).forEach(addStar);
+
+      // texture avatar
+      const avatarTexture = new THREE.TextureLoader().load("./dog.png");
+      const avatar = new THREE.Mesh(
+        new THREE.BoxGeometry(3, 3, 3),
+        new THREE.MeshBasicMaterial({ map: avatarTexture })
+      );
+      scene.add(avatar);
+
+      // scroll
+      const moveCamera = () => {
+        const t = document.body.getBoundingClientRect().top;
+        torusMesh.rotation.x += 0.05;
+        torusMesh.rotation.y += 0.075;
+        torusMesh.rotation.z += 0.05;
+
+        avatar.rotation.y += 0.01;
+        avatar.rotation.z += 0.01;
+
+        camera.position.z = t * -0.01;
+        camera.position.x = t * -0.0002;
+        camera.position.y = t * -0.0002;
+      };
+      document.body.onscroll = moveCamera;
+
+      // animate scene
+      const animate = () => {
+        requestAnimationFrame(animate);
+
+        // orbit controls
+        orbitControl.update();
+
+        // animate torus
+        torusMesh.rotation.x += 0.01;
+        torusMesh.rotation.z += 0.01;
+        torusMesh.rotation.y += 0.005;
         renderer.render(scene, camera);
-        requestAnimationFrame(renderScene);
       };
 
-      const handleResize = () => {
-        const width = window.innerWidth;
-        const height = window.innerHeight;
-
-        camera.aspect = width / height;
-        camera.updateProjectionMatrix();
-
-        renderer.setSize(width, height);
-      };
-
-      window.addEventListener("resize", handleResize);
-
-      // Call the renderScene function to start the animation loop
-      renderScene();
-
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
+      animate();
     }
   }, []);
 
